3519894f95907c73f718ef01e154e190051ed21d
jfathi <javidafathi@gmail.com>
2024-01-22 10:55:44 -0500

    Revert "Refactor naming conventions"
    This reverts commit e315e2d85e45afb5b18c313bf0fa4c847707e102.

--- webber/__init__.py
+++ webber/__init__.py
@@ -2,9 +2,10 @@
 Webber: A Pythonic executor framework aimed at making parallel computing easy for everyone.
 """
 from sys import platform as _platform
-from .core import DAG
-_VERSION = "0.0.1"
-_supported = ["linux", "linux2"]
+_VERSION = "0.0.2"
+_supported = ["linux", "linux2", "win32"]
 if _platform not in _supported:
     err_msg = f"Webber {_VERSION} is only supported on these platforms: {', '.join(_supported)}"
     raise NotImplementedError(err_msg)
+
+from .core import DAG
