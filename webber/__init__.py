1f3261de3f181345691d9797aa25df8669e91004
jfathi <javidafathi@gmail.com>
2024-06-23 12:02:03 -0400

    Update versioning, import Promise at module level

--- webber/__init__.py
+++ webber/__init__.py
@@ -2,12 +2,13 @@
 Webber: A Pythonic executor framework aimed at making parallel computing easy for everyone.
 """
 import sys as _sys
+import logging as _logging
 
-__version__ = '0.0.2'
+__version__ = '0.1.2'
 __supported__ = ("linux", "linux2", "win32")
 
 if _sys.platform not in __supported__:
-    err_msg = f"Webber {__version__} is only supported on these platforms: {', '.join(__supported__)}"
-    raise NotImplementedError(err_msg)
+    _logging.warn(f"Webber {__version__} is only supported on these platforms: {', '.join(__supported__)}")
 
 from .core import DAG, Condition
+from .xcoms import Promise
