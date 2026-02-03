08edd0eeec3e25db7bd8ac690a8e5ef58520d8ec
Javid Alexander Fathi <javidafathi@gmail.com>
2026-02-03 11:26:38 -0800

    Veresioning update

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 setup(
     name='webber',
-    version='0.2.0',
-    description='An Pythonic platform for no-latency ETL and parallelization inspired by Apache Airflow.',
-    long_description='An Pythonic platform for no-latency ETL and parallelization inspired by Apache Airflow.',
+    version='0.3.0',
+    description='A Pythonic platform for no-latency ETL and embarrassingly parallel directed acyclic graphs.',
+    long_description='A Pythonic platform for no-latency ETL and embarrassingly parallel directed acyclic graphs.',
     long_description_content_type='text/x-rst',
     packages=find_packages(include=['webber', 'webber.*']),
     install_requires=["networkx", "flask", "pyvis", "jinja2", "matplotlib", "ipympl", "netgraph"],
@@ -12,5 +12,5 @@ setup(
     url='https://www.webberproject.com',
     package_dir={'webber': 'webber'},
     package_data={'webber': ['viz/templates/*.html']},
-    classifiers=['Development Status :: 3 - Alpha'],
+    classifiers=['Development Status :: 4 - Beta'],
 )
